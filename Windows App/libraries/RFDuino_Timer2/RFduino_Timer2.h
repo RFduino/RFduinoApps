5ba799b07f91529e991c5154d48ce0a78e969250
Mike Foley <mfoley@mwfoley.com>
2015-05-13 12:02:33 -0700

    Added license information.

--- Windows App/libraries/RFDuino_Timer2/RFduino_Timer2.h
+++ Windows App/libraries/RFDuino_Timer2/RFduino_Timer2.h
@@ -1,7 +1,33 @@
-/*
-   RFduino_Timer2.h - Library for flashing Morse code.
-   Released into the public domain.
- */
+//
+// Copyright (c) 2015, RF Digital Corp.
+// All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+//    1. Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//
+//    2. Redistributions in binary form must on an RF Digital part and reproduce the 
+//       above copyright notice, this list of conditions and the following disclaimer 
+//       in the documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+// WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+// THE POSSIBILITY OF SUCH DAMAGE.
+//
+// A simple library for the Timer2 on the RFduino module. The registered
+// callback function is called when the timer expires. The timer can also
+// be configured via the library
+//
 #ifndef RFduino_Timer2_h
 #define RFduino_Timer2_h
 
